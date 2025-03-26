0b148ce1527d050c17d7bfcf65a52eaaf4713282
Ansh-eng-ui <182800262+Ansh-eng-ui@users.noreply.github.com>
2025-03-26 11:48:26 +0530

    Add files via upload

---
+++ master/subject.py
@@ -0,0 +1,29 @@
+from master import app, db
+
+## SUBJECT MODEL
+class Subject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    chapters = db.relationship('Chapter', backref='subject', lazy=True)
+
+## CHAPTER MODEL
+class Chapter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
+    quizzes = db.relationship('Quiz', backref='chapter', lazy=True)
+
+## QUIZ MODEL
+class Quiz(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    chapter_id = db.Column(db.Integer, db.ForeignKey('chapter.id'), nullable=False)
+    questions = db.relationship('Question', backref='quiz', lazy=True)
+
+## QUESTION MODEL
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(300), nullable=False)
+    answer = db.Column(db.String(100), nullable=False)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)
+
